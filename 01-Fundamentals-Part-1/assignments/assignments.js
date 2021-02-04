7de7be6a8589329ae8ea600915befc4c8acf567e
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-02-04 16:54:05 +0700

    Update assignments.js

--- 01-Fundamentals-Part-1/assignments/assignments.js
+++ 01-Fundamentals-Part-1/assignments/assignments.js
@@ -1,4 +1,4 @@
-// LECTURE: Values and Variables
+// ================LECTURE: Values and Variables===================
 // 1. Declare variables called 'country', 'continent' and 'population' and 
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
@@ -13,3 +13,24 @@ let population = '270.6 Million'
 // 2. Log their values to the console
 console.log("LECTURE: Values and Variables");
 console.log('Country = ', country, "\nContinent = ", continent, "\nPopulation = ", population);
+
+
+// ===================LECTURE: Data Types==================
+// 1. Declare a variable called 'isIsland' and set its value according to your 
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+//ANSWER
+// 1. Declare a variable called 'isIsland' and set its value according to your 
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+let isIsland = true
+let language
+
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+console.log('LECTURE: Data Types')
+console.log('isIsland = ', typeof isIsland, '\nPopulation = ', typeof population, '\nCountry = ',
+    typeof country, '\nLanguage = ', typeof language)

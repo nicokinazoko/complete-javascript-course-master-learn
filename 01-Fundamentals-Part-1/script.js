7e35a93eedb8265581b6f9eab1e4a3a1f74f8bd8
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-02-04 12:05:09 +0700

    Create script.js

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,24 @@
+        //declaration js is amazing
+        let js = 'amazing';
+
+        //make output calculation using console.log
+        console.log(40 + 8 + 23 - 10);
+
+        //output value Nico
+        console.log('Nico')
+
+        //output number
+        console.log(23)
+
+        //declaration name 
+        let firstName = 'Nico'
+
+        //output firstName 
+        console.log(firstName)
+
+        //declaration variable
+        let myFirstJob = 'Programmer'
+        let myCurrentJob = 'College Student'
+
+        //output variable
+        console.log(myFirstJob)

f82f320130f4b26d68d686949d5950d0d366d5a8
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-02-04 23:45:10 +0700

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -24,56 +24,82 @@
         // console.log(myFirstJob)
 
 
-        //====================DATA TYPES=====================
-        //**** 7 PRIMITIVE DATA TYPES *****
-        //1. Number : Floating point Numbers
-        //EXAMPLE
-        let number = 23
+        // //====================DATA TYPES=====================
+        // //**** 7 PRIMITIVE DATA TYPES *****
+        // //1. Number : Floating point Numbers
+        // //EXAMPLE
+        // let number = 23
 
-        //2. String : Sequence of characters
-        //example
-        let firstNameNico = 'Nico'
+        // //2. String : Sequence of characters
+        // //example
+        // let firstNameNico = 'Nico'
 
-        //3. Boolean : Logical type that can only be true or false
-        //example
-        let fullAge = true
+        // //3. Boolean : Logical type that can only be true or false
+        // //example
+        // let fullAge = true
 
-        //4. Undefined : Value taken by a variable that is not yet defined ('empty value')
-        //example
-        let children
+        // //4. Undefined : Value taken by a variable that is not yet defined ('empty value')
+        // //example
+        // let children
 
-        //5. Null : empty value
+        // //5. Null : empty value
        
-        //6. Symbol(ES2015) : Value that is unique and cannot be changed
+        // //6. Symbol(ES2015) : Value that is unique and cannot be changed
 
-        //7. BigInt(ES2020) : Larger integer than the Number type can hold
+        // //7. BigInt(ES2020) : Larger integer than the Number type can hold
 
 
-        //assign javascriptIsFun with boolean value
-        let javascriptIsFun = true
+        // //assign javascriptIsFun with boolean value
+        // let javascriptIsFun = true
 
-        //output javascriptIsFun
-        console.log(javascriptIsFun)
+        // //output javascriptIsFun
+        // console.log(javascriptIsFun)
 
-        //typeof : use for know the variable data types
+        // //typeof : use for know the variable data types
+        // //example
+        // console.log(typeof javascriptIsFun)
+
+        // //reassign javascriptIsFun
+        // javascriptIsFun = 'YES'
+
+        // //output javascriptIsFun data types
+        // console.log(typeof javascriptIsFun)
+
+        // //example empty value
+        // let year
+
+        // //output year
+        // console.log(year)
+        // console.log(typeof year)
+
+        // //reassign year
+        // year = 1991
+
+        // //output year data types
+        // console.log(typeof year)
+
+        
+
+        //================LET, CONST, AND VAR================================
+
+        //LET
+        //can change the values
+        //example 
+        let age = 30
+        age = 31
+
+        //CONST
+        //value cannot be changed
+        //value cannot be empty or not declared
         //example
-        console.log(typeof javascriptIsFun)
+        const birthYear = 1998
 
-        //reassign javascriptIsFun
-        javascriptIsFun = 'YES'
+        //VAR
+        //old, use let instead
+        //example
+        var job = 'progammer'
+        job = 'teacher'
 
-        //output javascriptIsFun data types
-        console.log(typeof javascriptIsFun)
-
-        //example empty value
-        let year
-
-        //output year
-        console.log(year)
-        console.log(typeof year)
-
-        //reassign year
-        year = 1991
-
-        //output year data types
-        console.log(typeof year)
+        //example without using let, const, and var
+        lastName = 'Nico'
+        console.log(lastName)

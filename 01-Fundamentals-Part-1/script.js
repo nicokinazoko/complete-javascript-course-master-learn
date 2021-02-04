4879d71c2c68bbb2962a8c536249cf39c3b4b7e7
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-02-04 16:54:01 +0700

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,24 +1,79 @@
-        //declaration js is amazing
-        let js = 'amazing';
+        // //declaration js is amazing
+        // let js = 'amazing';
 
-        //make output calculation using console.log
-        console.log(40 + 8 + 23 - 10);
+        // //make output calculation using console.log
+        // console.log(40 + 8 + 23 - 10);
 
-        //output value Nico
-        console.log('Nico')
+        // //output value Nico
+        // console.log('Nico')
 
-        //output number
-        console.log(23)
+        // //output number
+        // console.log(23)
 
-        //declaration name 
-        let firstName = 'Nico'
+        // //declaration name 
+        // let firstName = 'Nico'
 
-        //output firstName 
-        console.log(firstName)
+        // //output firstName 
+        // console.log(firstName)
 
-        //declaration variable
-        let myFirstJob = 'Programmer'
-        let myCurrentJob = 'College Student'
+        // //declaration variable
+        // let myFirstJob = 'Programmer'
+        // let myCurrentJob = 'College Student'
 
-        //output variable
-        console.log(myFirstJob)
+        // //output variable
+        // console.log(myFirstJob)
+
+
+        //====================DATA TYPES=====================
+        //**** 7 PRIMITIVE DATA TYPES *****
+        //1. Number : Floating point Numbers
+        //EXAMPLE
+        let number = 23
+
+        //2. String : Sequence of characters
+        //example
+        let firstNameNico = 'Nico'
+
+        //3. Boolean : Logical type that can only be true or false
+        //example
+        let fullAge = true
+
+        //4. Undefined : Value taken by a variable that is not yet defined ('empty value')
+        //example
+        let children
+
+        //5. Null : empty value
+       
+        //6. Symbol(ES2015) : Value that is unique and cannot be changed
+
+        //7. BigInt(ES2020) : Larger integer than the Number type can hold
+
+
+        //assign javascriptIsFun with boolean value
+        let javascriptIsFun = true
+
+        //output javascriptIsFun
+        console.log(javascriptIsFun)
+
+        //typeof : use for know the variable data types
+        //example
+        console.log(typeof javascriptIsFun)
+
+        //reassign javascriptIsFun
+        javascriptIsFun = 'YES'
+
+        //output javascriptIsFun data types
+        console.log(typeof javascriptIsFun)
+
+        //example empty value
+        let year
+
+        //output year
+        console.log(year)
+        console.log(typeof year)
+
+        //reassign year
+        year = 1991
+
+        //output year data types
+        console.log(typeof year)

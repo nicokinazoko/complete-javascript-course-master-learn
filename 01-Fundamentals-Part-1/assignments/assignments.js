38bdeba1138c529aebc315555b88267bf401b14a
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-02-04 12:05:12 +0700

    Create assignments.js

---
+++ 01-Fundamentals-Part-1/assignments/assignments.js
@@ -0,0 +1,15 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and 
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+//ANSWER
+// 1. Declare variables called 'country', 'continent' and 'population' and 
+// assign their values according to your own country (population in millions)
+let country = 'Indonesia';
+let continent = 'Asia'
+let population = '270.6 Million'
+
+// 2. Log their values to the console
+console.log("LECTURE: Values and Variables");
+console.log('Country = ', country, "\nContinent = ", continent, "\nPopulation = ", population);

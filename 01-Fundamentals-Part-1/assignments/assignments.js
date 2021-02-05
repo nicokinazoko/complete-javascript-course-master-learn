df38d68743b08cd3db3a32fb4408d6753d076a77
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-02-05 11:24:42 +0700

    Update assignments.js

--- 01-Fundamentals-Part-1/assignments/assignments.js
+++ 01-Fundamentals-Part-1/assignments/assignments.js
@@ -8,11 +8,11 @@
 // assign their values according to your own country (population in millions)
 let country = 'Indonesia';
 let continent = 'Asia'
-let population = '270.6 Million'
+let population = '270.6'
 
 // 2. Log their values to the console
-console.log("LECTURE: Values and Variables");
-console.log('Country = ', country, "\nContinent = ", continent, "\nPopulation = ", population);
+console.log("\nLECTURE: Values and Variables");
+console.log('\nCountry = ', country, "\nContinent = ", continent, "\nPopulation = ", population);
 
 
 // ===================LECTURE: Data Types==================
@@ -31,8 +31,8 @@ let language
 
 // 2. Log the types of 'isIsland', 'population', 'country' and 'language'
 // to the console
-console.log('LECTURE: Data Types')
-console.log('isIsland = ', typeof isIsland, '\nPopulation = ', typeof population, '\nCountry = ',
+console.log('\nLECTURE: Data Types')
+console.log('\nisIsland = ', typeof isIsland, '\nPopulation = ', typeof population, '\nCountry = ',
     typeof country, '\nLanguage = ', typeof language)
 
 
@@ -50,11 +50,53 @@ language = 'Indonesian'
 
 // 2. Think about which variables should be const variables (which values will never 
 //     change, and which might change?). Then, change these variables to const.
-const country = 'Indonesia'
-const continent = 'Java'
-const inIsland = true
+// const country = 'Indonesia'
+// const continent = 'Java'
+// const inIsland = true
 
 // 3. Try to change one of the changed variables now, and observe what happens
 inIsland = false
 
 
+console.log('\nLECTURE: Basic Operators')
+//===================LECTURE: Basic Operators================
+// 1. If your country split in half, and each half would contain half the population, 
+// then how many people would live in each half?
+// 2. Increase the population of your country by 1 and log the result to the console
+// 3. Finland has a population of 6 million. Does your country have more people than 
+// Finland?
+// 4. The average population of a country is 33 million people. Does your country 
+// have less people than the average country?
+// 5. Based on the variables you created, create a new variable 'description'
+// which contains a string with this format: 'Portugal is in Europe, and its 11 million 
+// people speak portuguese
+
+//ANWER
+// 1. If your country split in half, and each half would contain half the population, 
+// then how many people would live in each half?
+
+let populationInHalf = population / 2
+console.log('\nPeople would live in each half in population is : ', populationInHalf)
+
+// 2. Increase the population of your country by 1 and log the result to the console
+populationInHalf++
+console.log('Result of increase population by 1 : ', populationInHalf)
+
+// 3. Finland has a population of 6 million. Does your country have more people than 
+// Finland?
+let finlandPopulation = 6
+let isMorePeopleOrNot = population > finlandPopulation
+console.log('Indonesia has more people than Finland :', isMorePeopleOrNot)
+
+// 4. The average population of a country is 33 million people. Does your country 
+// have less people than the average country?
+
+let averagePopulation = 33
+let isLessPeopleOrNot = population < averagePopulation
+console.log('Indonesia has less people than the average country : ', isLessPeopleOrNot)
+// 5. Based on the variables you created, create a new variable 'description'
+// which contains a string with this format: 'Portugal is in Europe, and its 11 million 
+// people speak portuguese
+let description = country + ' is in ' + continent + ', and its ' + population +
+    ' million people speak ' + language
+console.log(description)

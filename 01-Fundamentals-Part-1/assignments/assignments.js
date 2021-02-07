11d7a411a42a4614896d6e592bedd4ca685ee32c
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-02-07 12:08:17 +0700

    Update assignments.js

--- 01-Fundamentals-Part-1/assignments/assignments.js
+++ 01-Fundamentals-Part-1/assignments/assignments.js
@@ -112,3 +112,68 @@ const descriptionNew = `${country} is in ${continent} and its ${population} mill
 console.log(descriptionNew)
 
 
+
+console.log(`LECTURE: Taking Decisions: if / else Statements`)
+// 1. If your country's population is greater that 33 million, log a string like this to the 
+// console: 'Portugal's population is above average'. Otherwise, log a string like 
+// 'Portugal's population is 22 million below average' (the 22 is the average of 33 
+// minus the country's population)
+// 2. After checking the result, change the population temporarily to 13 and then to 
+// 130. See the different results, and set the population back to original
+
+//ANSWER
+// 1. If your country's population is greater that 33 million, log a string like this to the 
+// console: 'Portugal's population is above average'. Otherwise, log a string like 
+// 'Portugal's population is 22 million below average' (the 22 is the average of 33 
+// minus the country's population)
+
+if (population > 33) {
+    console.log(`${country}'s population is above average`)
+} else {
+    console.log(`${country}'s population is ${33 - population} below average`)
+}
+
+// 2. After checking the result, change the population temporarily to 13 and then to 
+// 130. See the different results, and set the population back to original
+
+//if the population is 13
+population = 13
+
+if (population > 33) {
+    console.log(`${country}'s population is above average`)
+} else {
+    console.log(`${country}'s population is ${33 - population} below average`)
+}
+
+//if the population is 33
+population = 33
+if (population > 33) {
+    console.log(`${country}'s population is above average`)
+} else {
+    console.log(`${country}'s population is ${33 - population} below average`)
+}
+
+console.log(`LECTURE: Type Conversion and Coercion`)
+// 1. Predict the result of these 5 operations without executing them:
+// '9' - '5';
+// '19' - '13' + '17';
+// '19' - '13' + 17;
+// '123' < 57;
+// 5 + 6 + '4' + 9 - 4 - 2;
+// 2. Execute the operations to check if you were right
+
+//ANSWER
+// 1. Predict the result of these 5 operations without executing them:
+// '9' - '5'; = 4
+// '19' - '13' + '17'; 617
+// '19' - '13' + 17; 23
+// '123' < 57; false
+// 5 + 6 + '4' + 9 - 4 - 2; 1143
+// 2. Execute the operations to check if you were right
+console.log(`
+    ${'9' - '5'},
+    ${'19' - '13' + '17'},
+    ${'19' - '13' + 17},
+    ${'123' < 57},
+    ${5 + 6 + '4' + 9 - 4 - 2}
+    `)

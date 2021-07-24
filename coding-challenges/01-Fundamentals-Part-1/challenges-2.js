3e922d73a289ac193f8668f7f61ee288cbef850b
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-07-24 17:28:01 +0700

    Create challenges-2.js

---
+++ coding-challenges/01-Fundamentals-Part-1/challenges-2.js
@@ -0,0 +1,35 @@
+// console.log(`\nChallenge 2`)
+
+// //declare for result
+
+
+// //TEST DATA 1
+
+// //Data Mark
+// // const massMark = 78
+// // const heightMark = 1.69
+
+// // // //Data John
+// // const massJohn = 92
+// // const heightJohn = 1.95
+
+// //TEST DATA 2
+
+// //Data Mark
+// const massMark = 95
+// const heightMark = 1.88
+
+// //Data John
+// const massJohn = 85
+// const heightJohn = 1.76
+
+// //declare bmi to store BMI data
+// const bmiMark = massMark / heightMark ** 2
+// const bmiJohn = massJohn / heightJohn ** 2
+
+// //show result for mark bmi is higher than john with if
+// if (bmiMark > bmiJohn) {
+//     console.log(`Mark's BMI ${bmiMark} is higher than John's (${bmiJohn})!`)
+// } else {
+//     console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`)
+// }
